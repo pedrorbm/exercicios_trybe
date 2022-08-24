b9397f0702489f84e7107adb154d80152ed2301f
Pedro Roberto Bezerra De Medeiros <pedropwks@gmail.com>
2022-08-23 22:48:41 -0300

    Adiciona exercício dia 04, bloco 04, módulo 01

---
+++ 01-fundamentos_web/bloco-04-introducao_a_javascript_e_logica_de_programacao/dia-04-javascript_objetos_e_funcoes/exercicio.js
@@ -0,0 +1,59 @@
+//let info = {
+    //personagem: 'Margarida',
+    //origem: 'Pato Donald',
+    //nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//};
+
+//exercício 1
+//let saudacao = 'Bem-vinda, ' + info['personagem'];
+
+//console.log(saudacao);
+
+//exercício 2
+//info['recorrente'] = 'Sim';
+
+//console.log(info);
+
+//exercício 3
+//for (let key in info) {
+    //console.log(key);
+//};
+
+//exercício 4
+//for (let key in info) {
+    //console.log(info[key]);
+//};
+
+//exercício 5
+//let info = {
+    //personagem: 'Tio Patinhas',
+    //origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    //nota: 'O último MacPatinhas',
+    //recorrente: 'Sim'
+//};
+
+//for (let key in info) {
+    //console.log(key, info[key]);
+//};
+
+//exercício 6
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+let fullName = leitor['nome'] + ' ' + leitor['sobrenome'];
+let title = leitor['livrosFavoritos'][0]['titulo'];
+
+console.log('O livro favorito de ' + fullName + ' se chama ' + title + '.')
+
+//exercício 7
+leitor['livrosFavoritos'][]
