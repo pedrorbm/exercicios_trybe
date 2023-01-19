88b063c8fbb1e5d4c1e8a984753557cb783ff0ea
Pedro Roberto Bezerra De Medeiros <pedropwks@gmail.com>
2023-01-19 02:45:50 -0300

    Adiciona section 06 front-end

---
+++ 02-front-end/section-02-introducao-ao-react/dia-01-hello-world-no-react/exercicio/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <>
+      <h1>Exercise - Frontend content</h1>
+      <Header />
+      <Content />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
