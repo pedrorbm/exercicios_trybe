572198b1dc74a8d40d9c64e47bfff87c7e0df45b
Pedro Medeiros <pedropwks@gmail.com>
2023-06-14 12:07:37 -0300

    Adiciona exercício 4 - 03-04-02

--- 03-back-end/section-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-nodejs-api-rest-com-express/src/app.js
+++ 03-back-end/section-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-nodejs-api-rest-com-express/src/app.js
@@ -7,6 +7,11 @@ app.use(express.json());
 
 app.get('/', (req, res) => res.status(200).json({ message: 'funcionando' }));
 
+app.get('/movies', async (req, res) => {
+  const movies = await lecture();
+  res.status(200).json(movies);
+});
+
 app.get('/movies/:id', async (req, res) => {
   const movies = await lecture();
   const search = movies.find(({ id }) => id === Number(req.params.id));
