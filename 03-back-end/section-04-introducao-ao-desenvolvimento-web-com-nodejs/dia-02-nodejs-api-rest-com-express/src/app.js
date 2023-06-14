db38e45a6b838ec50d83e0b1d3cf1cb1b9ef8e58
Pedro Medeiros <pedropwks@gmail.com>
2023-06-14 13:17:47 -0300

    Finalizaçao correção - 03-04-02

--- 03-back-end/section-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-nodejs-api-rest-com-express/src/app.js
+++ 03-back-end/section-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-nodejs-api-rest-com-express/src/app.js
@@ -9,40 +9,58 @@ app.use(express.json());
 app.get('/', (req, res) => res.status(200).json({ message: 'funcionando' }));
 
 app.get('/movies', async (req, res) => {
-  const movies = await lecture();
-  res.status(200).json(movies);
+  try {
+    const movies = await lecture();
+    res.status(200).json(movies);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
 });
 
 app.get('/movies/:id', async (req, res) => {
-  const movies = await lecture();
-  const search = movies.find(({ id }) => id === Number(req.params.id));
-  res.status(200).json(search);
+  try {
+    const movies = await lecture();
+    const search = movies.find(({ id }) => id === Number(req.params.id));
+    res.status(200).json(search);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
 });
 
 app.post('/movies', async (req, res) => {
-  const movies = await lecture();
-  movies.push({ ...req.body });
-  fs.writeFile('./src/movies.json', JSON.stringify(movies));
-  res.status(201).json(movies);
+  try {
+    const movies = await lecture();
+    movies.push({ ...req.body });
+    await fs.writeFile('./src/movies.json', JSON.stringify(movies));
+    res.status(201).json(movies);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
 });
 
 app.put('/movies/:id', async (req, res) => {
-  const movies = await lecture();
-  const search = movies.find(({ movie }) => movie === req.params.id);
-
-  const { movie, price } = req.body;
-  search.movie = movie;
-  search.price = price;
-  fs.writeFile('./src/movies.json', JSON.stringify(movies));
-  res.status(200).json(movies);
+  try {
+    const movies = await lecture();
+    const search = movies.find(({ movie }) => movie === req.params.id);
+    const { movie, price } = req.body;
+    search.movie = movie;
+    search.price = price;
+    await fs.writeFile('./src/movies.json', JSON.stringify(movies));
+    res.status(200).json(movies);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
 });
 
 app.delete('/movies/:id', async (req, res) => {
-  const movies = await lecture();
-  const search = movies.filter(({ id }) => id !== Number(req.params.id));
-  console.log(search);
-  fs.writeFile('./src/movies.json', JSON.stringify(search));
-  res.status(200).json(search);
+  try {
+    const movies = await lecture();
+    const search = movies.filter(({ id }) => id !== Number(req.params.id));
+    await fs.writeFile('./src/movies.json', JSON.stringify(search));
+    res.status(204).end();
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
 });
 
 module.exports = app;
